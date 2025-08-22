07e8b3454458092e3ca5576e85d5d73169ca0d66
LeSabou <clement.sabourinpro@gmail.com>
2025-08-22 10:14:37 +0200

    V2 - Ajout des commentaires dans le code

--- index.py
+++ index.py
@@ -1,14 +1,18 @@
+# Import du module random pour le choix du nombre mystère
 import random 
 
+# Déclaration des différentes variables 
 mysteriousNum = random.randint(1, 50)
 tentativeNum = 0
 playerNum = 0
 title = "Bienvenue sur le jeux du nombre mystère !"
 chooseNum = "Veuillez choisir un nombre entre 1 et 50: "
 
+# Lancement du jeux
 print(title)
 input(chooseNum)
 
+# Boucle while pour le fonctionnement du jeux 
 while playerNum != mysteriousNum:
     if mysteriousNum > playerNum:
         print("Le nombre mystère est plus grand")
